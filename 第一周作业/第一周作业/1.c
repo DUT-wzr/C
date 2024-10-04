a6e6fc884b1433358f4d132e3d6a96cc7422fd62
sdkjhdfdg <2827019904@qq.com>
2024-10-04 12:57:00 +0800

    C语言第一周作业

---
+++ 第一周作业/第一周作业/1.c
@@ -0,0 +1,15 @@
+//Hello World(10分)
+//题目内容:在屏幕上输出一行信息:Hello World
+//输入格式:无
+//输出格式:
+//输入样例
+//输出样例:Hello World
+//
+
+
+//#include <stdio.h>
+//int main(){
+//	printf("Hello World");
+//	getchar();
+//	return 0;	
+//}
